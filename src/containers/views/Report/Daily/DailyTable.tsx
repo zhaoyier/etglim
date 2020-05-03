099685f7d368e62b5fbde0a555fb1d669aa7087b
赵建伟 <zhaojianwei@ezbuy.com>
2020-05-03 19:52:26 +0800

    订单状态

--- src/containers/views/Report/Daily/DailyTable.tsx
+++ src/containers/views/Report/Daily/DailyTable.tsx
@@ -13,9 +13,8 @@ function DailyTable({ scrollY }: IProps) {
     const { reportStore } = reportRootStore()
 
     const columns = [
-        { title: 'Variety', dataIndex: 'variety', key: 'variety', fixed: 'left' },
         { title: 'Date', dataIndex: 'date', key: 'date', fixed: 'left' },
-        { title: 'Weekday', dataIndex: 'date', key: 'date', fixed: 'left' },
+        { title: 'Weekday', dataIndex: 'weekday', key: 'weekday', fixed: 'left' },
         { title: 'opening', dataIndex: 'opening', key: 'opening' },
         { title: 'highest', dataIndex: 'highest', key: 'highest' },
         { title: 'lowest', dataIndex: 'lowest', key: 'lowest' },
@@ -23,8 +22,7 @@ function DailyTable({ scrollY }: IProps) {
         { title: 'todaySettlement', dataIndex: 'todaySettlement', key: 'todaySettlement' },
         { title: 'volume', dataIndex: 'volume', key: 'volume' },
         { title: 'emptyVolume', dataIndex: 'emptyVolume', key: 'emptyVolume' },
-        { title: 'changes', dataIndex: 'changes', key: 'changes' },
-        { title: 'Action', key: 'operation', fixed: 'right', width: 100, render: (_, record) => {} }
+        { title: 'changes', dataIndex: 'changes', key: 'changes' }
     ]
 
     return (
